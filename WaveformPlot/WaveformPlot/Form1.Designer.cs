a0073b08c0b2880142b2599140151fe7e99dfc0b
ParrotShen <parrotshen@yahoo.com.tw>
2023-05-31 17:35:34 +0800

    IQ waveform plot

---
+++ WaveformPlot/WaveformPlot/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WaveformPlot
+{
+    partial class FormPlot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartWav = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.buttonFile = new System.Windows.Forms.Button();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chartWav)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartWav
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartWav.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartWav.Legends.Add(legend1);
+            this.chartWav.Location = new System.Drawing.Point(12, 41);
+            this.chartWav.Name = "chartWav";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartWav.Series.Add(series1);
+            this.chartWav.Size = new System.Drawing.Size(608, 395);
+            this.chartWav.TabIndex = 0;
+            this.chartWav.Text = "chart1";
+            // 
+            // buttonFile
+            // 
+            this.buttonFile.Location = new System.Drawing.Point(12, 12);
+            this.buttonFile.Name = "buttonFile";
+            this.buttonFile.Size = new System.Drawing.Size(100, 23);
+            this.buttonFile.TabIndex = 1;
+            this.buttonFile.Text = "Open File";
+            this.buttonFile.UseVisualStyleBackColor = true;
+            this.buttonFile.Click += new System.EventHandler(this.buttonFile_Click);
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Location = new System.Drawing.Point(118, 10);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.Size = new System.Drawing.Size(502, 25);
+            this.textBoxFile.TabIndex = 2;
+            // 
+            // FormPlot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 448);
+            this.Controls.Add(this.textBoxFile);
+            this.Controls.Add(this.buttonFile);
+            this.Controls.Add(this.chartWav);
+            this.Name = "FormPlot";
+            this.Text = "WaveformPlot";
+            this.Load += new System.EventHandler(this.FormPlot_Load);
+            this.Resize += new System.EventHandler(this.FormPlot_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.chartWav)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartWav;
+        private System.Windows.Forms.Button buttonFile;
+        private System.Windows.Forms.TextBox textBoxFile;
+    }
+}
+
---
+++ WaveformPlot/WaveformPlot/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WaveformPlot
+{
+    public partial class FormPlot : Form
+    {
+        private Rectangle orgFormSize;
+        private Rectangle orgChartSize;
+        private Rectangle orgTextBoxSize;
+
+        public FormPlot()
+        {
+            InitializeComponent();
+
+            chartWav.ChartAreas[0].AxisX.MajorGrid.LineWidth = 0;
+            chartWav.ChartAreas[0].AxisY.MajorGrid.LineWidth = 0;
+        }
+
+        private void PlotWaveform(string fileName)
+        {
+            byte[] data = File.ReadAllBytes( fileName );
+            Int32 samples = data.Length / 4;
+            if (samples > 1600000) samples = 1600000;
+
+            chartWav.Series[0].Points.Clear();
+            chartWav.ChartAreas[0].AxisX.LabelStyle.Format = "N0";
+            chartWav.ChartAreas[0].AxisY.LabelStyle.Format = "N0";
+
+            for (Int32 i = 0; i < samples; i++)
+            {
+                Int32 x = i + 1;
+                Int16 y = System.BitConverter.ToInt16(data, (i * 4));
+                chartWav.Series[0].Points.AddXY(x, y);
+            }
+        }
+
+        private void buttonFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Select file";
+                dlg.InitialDirectory = ".\\";
+                dlg.Filter = "Binary files (*.bin)|*.bin|All files (*.*)|*.*";
+                dlg.RestoreDirectory = true;
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    Cursor.Current = Cursors.WaitCursor;
+
+                    textBoxFile.Text = dlg.FileName;
+                    PlotWaveform(dlg.FileName);
+
+                    Cursor.Current = Cursors.Default;
+                }
+            }
+        }
+
+        private void FormPlot_Load(object sender, EventArgs e)
+        {
+            orgFormSize = new Rectangle(this.Location.X, this.Location.Y, this.Size.Width, this.Size.Height);
+            orgChartSize = new Rectangle(chartWav.Location.X, chartWav.Location.Y, chartWav.Size.Width, chartWav.Size.Height);
+            orgTextBoxSize = new Rectangle(textBoxFile.Location.X, textBoxFile.Location.Y, textBoxFile.Size.Width, textBoxFile.Size.Height);
+        }
+
+        private void FormPlot_Resize(object sender, EventArgs e)
+        {
+            float xRatio = (float)(this.Size.Width) / (float)(orgFormSize.Width);
+            float yRatio = (float)(this.Size.Height) / (float)(orgFormSize.Height);
+            int newWidth;
+            int newHeight;
+
+            //newWidth = (int)(orgTextBoxSize.Width * xRatio);
+            //textBoxFile.Size = new Size(newWidth, orgTextBoxSize.Height);
+
+            newWidth = (int)(orgChartSize.Width * xRatio);
+            newHeight = (int)(orgChartSize.Height * yRatio);
+            chartWav.Size = new Size(newWidth, newHeight);
+
+            newWidth = chartWav.Size.Width - (textBoxFile.Location.X - chartWav.Location.X);
+            textBoxFile.Size = new Size(newWidth, orgTextBoxSize.Height);
+        }
+    }
+}
